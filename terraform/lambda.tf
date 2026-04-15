34e047e1f185cfd7009b1507e8af624fb5385055
Angel Ballesteros <angelb@237global.com>
2026-04-15 22:54:22 +0200

    chore: reduce CloudWatch logs retention from 14 to 3 days

--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -81,5 +81,5 @@ resource "aws_lambda_function" "weather_extractor" {
 
 resource "aws_cloudwatch_log_group" "lambda_logs" {
   name              = "/aws/lambda/${var.lambda_function_name}"
-  retention_in_days = 14
+  retention_in_days = 3
 }
