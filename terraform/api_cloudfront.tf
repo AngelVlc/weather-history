eb2cf2392ec4eb55f6139cd2541bb978f802f452
Angel Ballesteros <angelb@237global.com>
2026-04-24 22:35:53 +0200

    Create custom cache policy for API to include days and until in cache key

--- terraform/api_cloudfront.tf
+++ terraform/api_cloudfront.tf
@@ -3,6 +3,17 @@ data "aws_lambda_function_url" "lambda_api" {
   function_name = var.lambda_api_function_name
 }
 
+resource "aws_cloudfront_cache_policy" "api" {
+  name       = "weather-api-cache-policy"
+  description = "Cache policy for weather API that includes days and until query strings"
+  
+  parameters_config {
+    query_string_cache_keys = ["days", "until"]
+    cookies_config = "none"
+    headers_config = "none"
+  }
+}
+
 resource "aws_cloudfront_distribution" "api" {
   origin {
     domain_name = trimsuffix(replace(data.aws_lambda_function_url.lambda_api.function_url, "https://", ""), "/")
@@ -25,7 +36,7 @@ resource "aws_cloudfront_distribution" "api" {
     target_origin_id          = "lambda-api-origin"
     viewer_protocol_policy   = "redirect-to-https"
     compress                = true
-    cache_policy_id         = "658327ea-f89d-4fab-a63d-7e88639e58f6"
+    cache_policy_id         = aws_cloudfront_cache_policy.api.id
     origin_request_policy_id = "b689b0a8-53d0-40ab-baf2-68738e2966ac"
   }
 
